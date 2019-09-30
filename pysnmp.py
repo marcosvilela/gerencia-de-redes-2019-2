c504c3a45d37e5993efd7e3afff3d870c765673e
marcosvilela <marcos.vilela42@hotmail.com>
2019-09-30 06:52:56 -0300

    codigo atualizado 30/09/2019

--- pysnmp.py
+++ pysnmp.py
@@ -3,12 +3,12 @@
 import socket
 import sys
 
-def formata_oid(OID_input):
+def formata_oid(oid_input):
 	
-	OID_tmp = OID_input.replace('.','')[2:]
+	oid_tmp = oid_input.replace('.','')[2:]
 
 	oid = chr(0x2b)
-	for i in OID_tmp:
+	for i in oid_tmp:
 		oid = oid + chr(int(i))
 
 	return oid
@@ -30,41 +30,41 @@ def monta_snmp(oid):
 	var_bind_list = type_var_bind_list + chr(len(var_bind)) + var_bind
 
 	#Request ID, Error, ErrorIndex
-	RqID = chr(2) + chr(1) + chr(1)
-	Err = chr(2) + chr(1) + chr(0)
-	ErrIndex = chr(2) + chr(1) + chr(0)
+	rqID = chr(2) + chr(1) + chr(1)
+	err = chr(2) + chr(1) + chr(0)
+	errIndex = chr(2) + chr(1) + chr(0)
 
-	SPDU = chr(0xa0) + chr(3 + 3 + 3 + len(var_bind_list)) + RqID + Err + ErrIndex + var_bind_list
+	spdu = chr(0xa0) + chr(3 + 3 + 3 + len(var_bind_list)) + rqID + err + errIndex + var_bind_list
 	
 	##Community, mantemos travado em public
 	community = 'public'
 	len_community = len(community)
 	comm = chr(4) + chr(len_community) + community
 
-	versao = chr(2) + chr(1) + chr(0)
+	version = chr(2) + chr(1) + chr(0)
 	
 	##Snmp Message
 	msg_type = chr(0x30)
-	snmp = msg_type + chr(3 + 2 + len_community + len(SPDU)) + versao + comm + SPDU
+	snmp = msg_type + chr(3 + 2 + len_community + len(spdu)) + version + comm + spdu
 	return snmp
 
-def send_socket_message(message, host='127.0.0.1'):
+def send_socket_msg(message, host='127.0.0.1'):
 	
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.settimeout(10)
 
-	s.sendto(message, (host, 161))
-	print "Quadro enviado... Aguardando resposta" 
+	s.sendto(message.encode(), (host, 161))
+	print("Quadro enviado... Aguardando resposta") 
 
 	while True:
 		
 		try:
 			rx_buf = s.recv(2000)
-			print "Recebido!" 
+			print("Recebido!") 
 			print(list(rx_buf))
 
 		except socket.timeout:
-			print "Timed out!" 
+			print("Timed out!") 
 			exit()
 		S = rx_buf[0]
 		break
@@ -77,15 +77,15 @@ def send_socket_message(message, host='127.0.0.1'):
 if __name__ == '__main__':
 
 	if len(sys.argv) < 2:
-		print "Numero incorreto de argumentos" 
-		print "Execute novamente no formato: pysnmp.py oid [oid...]" 
+		print("Numero incorreto de argumentos") 
+		print("Execute novamente no formato: pysnmp.py oid [oid...]") 
 		exit(-1)
 
 
 	for oid_input in sys.argv[1:]:
 
-		print "Enviando oid: " + str(oid_input)
+		print("Enviando oid: " + str(oid_input))
 		oid = formata_oid(oid_input)
 		snmp_message = monta_snmp(oid)
-		print "SNMP Message: " + str(list(snmp_message))
-		send_socket_message(snmp_message)
+		print("SNMP Message: " + str(list(snmp_message)))
+		send_socket_msg(snmp_message)
